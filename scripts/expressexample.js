4048ede7c3f0769d8d295955b8402a397b0e78b8
Sam Couch <samcouch91@gmail.com>
2015-09-14 20:26:20 -0400

    add channel

--- scripts/expressexample.js
+++ scripts/expressexample.js
@@ -5,4 +5,11 @@ module.exports = function(robot){
     robot.messageRoom('#'+channel, 'Anyone else feeling a little ' + expression + '?');
     res.send('You got it!')
   })
+  robot.router.get('/say/:channel/:comment', function(req, res){
+    var channel = req.params.channel
+    var comment = req.params.comment
+    robot.messageRoom('#'+channel, comment);
+    res.send('You got it!')
+  })
+
 }
