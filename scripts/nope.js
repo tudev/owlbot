d9e92bded3fc43a6417fd1caaf5e5e4262a04a6e
Sam Couch <samcouch91@gmail.com>
2015-09-16 17:46:40 -0400

    nope x2

--- scripts/nope.js
+++ scripts/nope.js
@@ -4,6 +4,10 @@
 
 module.exports = function(robot){
   robot.hear(/^([\s\w'@.-:]*)\s*([-+]{2}|—)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$/i, function(msg){
-    msg.send("Nope. https://github.com/tudev/owlbot/issues")
+    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
+  })
+
+  robot.hear(/(top|bottom) (\d+)/i, function(msg){
+    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
   })
 }
