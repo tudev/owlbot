7a1ce9774b31a58de587007c59637b84882a512f
wpower12 <willpowe@gmail.com>
2015-10-08 11:38:07 -0400

    Revert "Adding cellauto, plusplus"
    This reverts commit 0e950ee3cadb4d7c369504229a94f3ce67ec1396.

--- scripts/nope.js
+++ scripts/nope.js
@@ -3,11 +3,11 @@
 //
 
 module.exports = function(robot){
-//  robot.hear(/^([\s\w'@.-:]*)\s*([-+]{2}|???)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$/i, function(msg){
-//    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
-//  })
-//
-//  robot.hear(/(top|bottom) (\d+)/i, function(msg){
-//    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
-//  })
+  robot.hear(/^([\s\w'@.-:]*)\s*([-+]{2}|—)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$/i, function(msg){
+    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
+  })
+
+  robot.hear(/(top|bottom) (\d+)/i, function(msg){
+    msg.send("Nope. https://github.com/tudev/owlbot/issues/8")
+  })
 }
