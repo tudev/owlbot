c21ce58564ab271baf338d4df3f9d10db1d005c4
raman <ramanjit456@yahoo.co.in>
2015-10-28 13:38:04 -0400

    generated unique form URL per user

--- scripts/pm-setup.js
+++ scripts/pm-setup.js
@@ -16,7 +16,20 @@ module.exports = function(robot){
 		var userString = [];
 		var topActive = _.sortBy(getActiveUsers(), function(user){
     		console.log(user.name);
-    		userString.push(user.name);
+    		userString.push(user.name + ' ' + user.id  );
+    		return user;
+    	});
+    	console.log(topActive);
+    	console.log(userString);
+    	
+    	return userString;
+	}
+
+	function getUsersId(){
+		var userString = [];
+		var topActive = _.sortBy(getActiveUsers(), function(user){
+    		console.log(user.id);
+    		userString.push(user.id);
     		return user;
     	});
     	console.log(topActive);
@@ -28,10 +41,11 @@ module.exports = function(robot){
 	robot.respond(/message active/i, function(msg){
 		msg.send("i have to message " + getUsers());
 		_.each(getUsers(), function(user){
-			console.log("messaging "+user);
+			var userInfo = user.split(' ');
+			console.log("messaging "+userInfo[0]);
 			robot.send({
-			  room: user
-			}, "Hello! Can you take a survey for us?");
+			  room: userInfo[0]
+			}, "Hello! Can you take a survey for us? https://docs.google.com/a/temple.edu/forms/d/1c6SUzjE4VUpiVNQMDtrm1-opT7EzZCxh90wdGZxhVEQ/alreadyresponded?entry.2032465319=" + userInfo[1]);
 		});
 		/*robot.send({
 		  room: message.envelope.user.name
