9714055726a07c6b2ba69b55a5959944d4d16ee9
raman <ramanjit456@yahoo.co.in>
2015-11-07 16:02:32 -0500

    refactor and added sent list for response received

--- scripts/pm-setup.js
+++ scripts/pm-setup.js
@@ -18,7 +18,10 @@ module.exports = function(robot){
 		var userString = [];
 		var topActive = _.sortBy(getActiveUsers(), function(user){
     		console.log(user.name);
-    		userString.push(user.name + ' ' + user.id  );
+    		userString.push({
+		        id: user.id,
+		        name: user.name
+		      });
     		return user;
     	});
     	console.log(topActive);
@@ -30,15 +33,15 @@ module.exports = function(robot){
 
 	robot.respond(/message active/i, function(msg){
 		_.each(getUsers(), function(user){
-			var userInfo = user.split(' ');
-			console.log("messaging "+userInfo[0]);
 			
 			
 			if( WATCH_ROOMS.indexOf(msg.message.room) != -1 ){
-				msg.send("i have to message " + userInfo[0]);
+				msg.send("i have to message " + user.name);
 				robot.send({
-				  	room: userInfo[0]
-					}, "Hello! Can you take a survey for us? https://docs.google.com/a/temple.edu/forms/d/1c6SUzjE4VUpiVNQMDtrm1-opT7EzZCxh90wdGZxhVEQ/alreadyresponded?entry.2032465319=" + userInfo[1]);
+				  	room: user.name
+					}, "Hello! Can you take a survey for us? https://docs.google.com/a/temple.edu/forms/d/1c6SUzjE4VUpiVNQMDtrm1-opT7EzZCxh90wdGZxhVEQ/alreadyresponded?entry.2032465319=" + user.id);
+				// TODO add function to update sent list
+				sentList(user);
 			} else {
 				robot.send({
 				  	room: msg.message.room
@@ -47,5 +50,42 @@ module.exports = function(robot){
 				throw ExceptionError;//short out of foreach
 			}	
 		});
+		
 	});
+	robot.respond(/clearAll/i, function(msg){
+		robot.brain.set("sent", [] );
+	});
+
+	function getDevList(){
+		return robot.brain.get("sent") || [];
+	}
+
+	function sentList(user){
+		var List = getDevList();
+		console.log(List);
+		if( _.isUndefined(List ) ){
+			//new list
+
+			var users = getActiveUsers();
+		    var _user = _.find(users, {id: user.id});
+
+		    if(_.isUndefined(_user)){
+		      users.push({
+		        id: user.id,
+		        name: user.name
+		      });
+		  	}
+
+			robot.brain.set("sent", users);
+		} else {
+			var newUser = _.find(List, {id: user.id});
+			console.log("user  "+user);
+			console.log("newUser "+newUser);
+			if( _.isUndefined(newUser) ){
+				List.push(user);
+				robot.brain.set("sent", List);
+			}
+		}
+
+	}
 }
