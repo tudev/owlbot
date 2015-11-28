476fda18e8152ba0c4a976ed35e18ab91cbcfffb
Sam Couch <samcouch91@gmail.com>
2015-11-28 14:45:14 -0500

    idk

--- scripts/clear_redis.js
+++ scripts/clear_redis.js
@@ -2,4 +2,5 @@ module.export = function(robot){
     robot.brain.remove("last_survey_repsonse")
     robot.brain.remove("tudev_mentors")
     robot.brain.remove("sent")
+    console.log('working')
 }
