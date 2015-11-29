b1350b96b9d1439434fa022caa661dd246f50be8
Sam Couch <samcouch91@gmail.com>
2015-11-29 01:16:27 -0500

    just work

--- scripts/clear_redis.js
+++ scripts/clear_redis.js
@@ -1,8 +1,5 @@
 module.exports = function(robot){
     robot.respond(/killemwithfire/i, function(msg){
-        robot.brain.remove("last_survey_repsonse")
-        robot.brain.remove("tudev_mentors")
-        robot.brain.remove("sent")
         console.log('working')
     }
 }
