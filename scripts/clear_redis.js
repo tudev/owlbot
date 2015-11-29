e1791be2b4759eb2be7bacadff0132ae5c9f44a3
Sam Couch <samcouch91@gmail.com>
2015-11-29 01:28:13 -0500

    hm

--- scripts/clear_redis.js
+++ scripts/clear_redis.js
@@ -1,7 +1,6 @@
 module.exports = function(robot){
     robot.respond(/killemall/i, function(msg){
-        robot.brain.remove('tudev_mentors')
-        robot.brain.remove('last_survey_repsonse')
+        robot.brain.remove('log')
         robot.brain.remove('plusPlus')
         console.log('working')
     })
